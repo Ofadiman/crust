c14882a78e6f4bd0b3287bdafc64efa3e61fb31e
ofadiman <ofadiman@gmail.com>
2024-10-20 17:53:16 +0200

    implement users crud

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
 mod health;
+mod users;
 
-use actix_web::{App, HttpServer};
+use std::sync::Mutex;
+
+use actix_web::{web, App, HttpServer};
 use health::healthz;
+use users::User;
+
+pub struct State {
+    users: Mutex<Vec<User>>,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(healthz))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    let state = web::Data::new(State {
+        users: Mutex::new(Vec::new()),
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(state.clone())
+            .service(healthz)
+            .service(users::get_user_by_id)
+            .service(users::create_user)
+            .service(users::get_users)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
