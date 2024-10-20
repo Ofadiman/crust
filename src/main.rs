bedcbdbeb44f2b32c47ea63cea206608be53bf72
ofadiman <ofadiman@gmail.com>
2024-10-20 21:29:52 +0200

    add update user by id endpoint

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod users_domain;
 mod users_handle_create_user;
 mod users_handle_get_user_by_id;
 mod users_handle_paginate_users;
+mod users_handle_update_user_by_id;
 
 use std::sync::Mutex;
 
@@ -26,6 +27,7 @@ async fn main() -> std::io::Result<()> {
             .service(users_handle_get_user_by_id::handle_get_user_by_id)
             .service(users_handle_paginate_users::handle_paginate_users)
             .service(users_handle_create_user::handle_create_user)
+            .service(users_handle_update_user_by_id::handle_update_user_by_id)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
