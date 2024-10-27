72951f09fdb724af857664c27ea75e1c330be0bb
ofadiman <ofadiman@gmail.com>
2024-10-27 16:15:39 +0100

    create nested modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 pub mod health;
+pub mod sessions {
+    pub mod hello;
+}
 pub mod settings;
 pub mod state;
 pub mod udemy;
@@ -7,3 +10,6 @@ pub mod users_handle_create_user;
 pub mod users_handle_get_user_by_id;
 pub mod users_handle_paginate_users;
 pub mod users_handle_update_user_by_id;
+pub mod users {
+    pub mod hello;
+}
